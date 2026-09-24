7d2f855ebbff77da43f3b42548379eaede96b886
Bezruchko <mnt_13@mail.ru>
2026-09-24 16:16:55 +0700

    Simple hello screen

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Livewire\Login;
+use App\Livewire\Welcome;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', Welcome::class);
+Route::get('/login', Login::class)->name('login');
